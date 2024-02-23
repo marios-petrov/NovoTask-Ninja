9d4f726f4ff507fd672fee17ef0631b13706fc5c
Daniel <60050273+ledaniel03@users.noreply.github.com>
2024-02-23 15:58:03 -0500

    Commented store.js

--- NovoTaskNinja/static/js/calendar/store.js
+++ NovoTaskNinja/static/js/calendar/store.js
@@ -5,22 +5,22 @@ import { signal } from 'https://cdn.jsdelivr.net/npm/preact-htm-signals-standalo
  */
 
 /**@type {{value:number}} */
-export const monthSig = signal(1)
+export const monthSig = signal(1)  // stores the current month show on calender
 
 /**@type {{value:number}} */
-export const yearSig = signal(2024)
+export const yearSig = signal(2024)  // stores the current year shown on calender
 
 /**@type {{value:number}} */
-export const dateSig = signal(null)
+export const dateSig = signal(null)  // stores the current date shown on calender
 
 /**@type {{value: {id:number, type:string,title:string,time:number,freq:string,desc:string}[]}} */
 export const tasksSig = signal([])
 
 /**@type {{value:[string, number]}} */
-export const selectedDate = signal(['MONTH', '15'])
+export const selectedDate = signal(['MONTH', '15'])  // used to determine the Events popup to show
 
 /**@type {{value:number}} */
-export const semSig = signal(0)
+export const semSig = signal(0)  // used t determine the sem to show Spring or fall
 
 /**@type {{value:{open:boolean, type:string,message:string}}} */
-export const messageSig = signal({ open: false, type: '', message: "" })
+export const messageSig = signal({ open: false, type: '', message: "" })  // used to show and hide care and lunch messages
